abb47d2578305e62729774cadb471f2aba96f64c
SidWu <sidwuu@163.com>
2021-04-11 15:05:56 +0800

    物资代码CRUD

--- src/main/java/com/sid/xk/shake/basic/product/service/IProductService.java
+++ src/main/java/com/sid/xk/shake/basic/product/service/IProductService.java
@@ -1,7 +1,9 @@
 package com.sid.xk.shake.basic.product.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.sid.xk.shake.basic.product.entity.BasicProduct;
+import com.sid.xk.shake.basic.product.vo.ProductQuery;
 
 /**
  * <p>
@@ -12,5 +14,37 @@ import com.sid.xk.shake.basic.product.entity.BasicProduct;
  * @since 2021-03-28
  */
 public interface IProductService extends IService<BasicProduct> {
+
+    /**
+     * 分页查询
+     * @param form 查询条件
+     * @return Page<Company>
+     */
+    Page<BasicProduct> queryPage(ProductQuery form);
+
+    /**
+     * 查询详情
+     * @param productCode 物资代码
+     * @return
+     */
+    BasicProduct getBean(String productCode);
+
+    /**
+     * 新增
+     * @param mod 保存bean
+     */
+    void insert(BasicProduct mod);
+
+    /**
+     * 修改
+     * @param mod 保存bean
+     */
+    void update(BasicProduct mod);
+
+    /**
+     * 删除
+     * @param productCode 物资代码
+     */
+    void delete(String productCode);
 
 }
